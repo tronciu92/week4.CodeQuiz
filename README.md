b9f5d916870cee23c1cc3ce92fb5bea6db15d546
Vasile <tronciu92@gmail.com>
2020-07-13 11:37:02 -0700

    Week 4 HomeWork

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# week4.CodeQuiz
+# Code Quiz
+
+This is a quiz game about Javascript featuring ten questions. Link to project: <a href="" target="_blank">Click here</a>
+
+
+## About
+
+When starting the game, the user is presented with a question with 4 multiple choice answers to choose from. 
+
+Choosing a correct answer adds five seconds to the game timer. Choosing an incorrect answer deducts fifteen seconds from the game timer.
+
+Each game allows for 150 seconds of play. The game is over when the user answers all ten questions or the timer runs out.
+
+The user's score is how much time is left in the game, plus or minus the seconds that are added to the timer based on the answers chosen.
+
+After a user completes the game, they can enter in their name and submit it with their score. They can view all of the scores that are submitted by clicking on "View High Scores" on the top left of the page.
+
---
+++ assets/questions.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+      title: "Which method would you use to find an ID element?",
+      choices: ["getElementsById()", "getElementByID()", "getElementbyId()", "getElementsById()"],
+      answer: "getElementbyId()"
+    },
+    {
+      title: "To see if two variables are equal in an if / else statement you would use ____.",
+      choices: ["=", "==", "'equals'", "!="],
+      answer: "=="
+    },
+    {
+      title: "Math.random() returns ____.",
+      choices: ["a number between 1 and 9", "a number between 0 and 9", "a number between 0 and 1", "a number between 0 and 99"],
+      answer: "a number between 0 and 1"
+    }, 
+    {
+      title: "The appendChild() method places a node as the ____ child.",
+      choices: ["first", "last place you left off", "random", "last"],
+      answer: "last"
+    }, 
+    {
+      title: "The first index of an array is ____.",
+      choices: ["0", "1", "6", "custom"],
+      answer: "0"
+    }, 
+    {
+      title: "Javascript was created by ____.",
+      choices: ["Jeffrey Javascript", "Brendan Eich", "Who F. Cares Jr.", "Ned Flanders"],
+      answer: "Brendan Eich"
+    }, 
+    {
+      title: "What is not an example of an HTML event?",
+      choices: ["User hitting a key", "User clicking a mouse", "Web page loading", "When my dad yells at me"],
+      answer: "When my dad yells at me"
+    }, 
+    {
+      title: "What are Javascript's Boolean data type values?",
+      choices: ["skibby and hibby", "property and key name", "Tom and Jerry", "true and false"],
+      answer: "true and false"
+    }, 
+    {
+      title: "Javascript variables can be ______ ",
+      choices: ["globally scoped", "function scoped", "locally scoped", "all of the above"],
+      answer: "all of the above"
+    }, 
+    {
+      title: "'I like the movie Cats' can be considered _____ in Javascript",
+      choices: ["a string", "a number", "a boolean value", "a shit opinion"],
+      answer: "a string"
+    }
+  ];
